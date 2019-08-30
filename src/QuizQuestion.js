eb0b3b142299c263f04edfe27acb7cbe7f209ba3
Charissa Johnson <charissa.y.johnson@gmail.com>
2019-08-29 22:04:15 -0500

    add errors

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
-   handleClick(buttonText){
-    if(buttonText === this.props.quiz_question.answer){
+  constructor(props) {
+    super(props);
+    this.state = { incorrectAnswer: false };
+  }
+
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.setState({ incorrectAnswer: false });
       this.props.showNextQuestionHandler();
+    } else {
+      this.setState({ incorrectAnswer: true });
     }
   }
-  
+
   render() {
     return (
       <main>
@@ -29,6 +37,7 @@ class QuizQuestion extends Component {
             )}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
       </main>
     );
   }
